9b0090708afa3fc49b3b90d094b91c0259b5a208
shree mishra <shree.mishra25@gmail.com>
2021-08-23 20:07:23 +0530

    showTime.js

---
+++ src/common/showTime.js
@@ -0,0 +1,19 @@
+let showTime = [{
+    id:1,
+    showTime: "12:00PM"
+  },
+  {
+    id:2,
+    showTime: "3:00PM"
+  },
+  {
+    id:3,
+    showTime: "6:00PM"
+  },
+  {
+    id:4,
+    showTime: "9:00PM"
+  }
+]
+
+export default showTime;
