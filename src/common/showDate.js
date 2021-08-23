d87bce3511d99baa487fa7e1d4ba398610eb3457
shree mishra <shree.mishra25@gmail.com>
2021-08-23 16:24:29 +0530

    shoeDate.js

---
+++ src/common/showDate.js
@@ -0,0 +1,23 @@
+let showDate = [{
+    id:1,
+    showDate: "1/8/2021"
+  },
+  {
+    id:2,
+    showDate: "1/9/2021"
+  },
+  {
+    id:3,
+    showDate: "1/10/2021"
+  },
+  {
+    id:4,
+    showDate: "1/11/2021"
+  },
+  {
+    id:5,
+    showDate: "1/12/2021"
+  }
+]
+
+export default showDate;
